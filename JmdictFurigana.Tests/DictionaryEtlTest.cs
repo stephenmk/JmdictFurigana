4c80387d5cb5a85414f747c2b3db26a825fd5075
stephenmk <stephenmk@users.noreply.github.com>
2025-08-14 17:20:13 -0500

    Add comments

--- JmdictFurigana.Tests/DictionaryEtlTest.cs
+++ JmdictFurigana.Tests/DictionaryEtlTest.cs
@@ -12,15 +12,33 @@ public class DictionaryEtlTest
     [TestMethod]
     public async Task ExecuteTestWaruguchiFourReadings()
     {
+        /*
+            `Waruguchi.xml` contains an entry with the following forms and info tags:
+
+            Kanji Forms
+            悪口；惡口[oK]；わる口；悪ぐち[sK]
+            Readings
+            あっこう[悪口,惡口]；わるくち；わるぐち；あかくち[nokanji]；あかぐち[sk]
+
+            The reading あっこう is restricted to forms 悪口 and 惡口.
+            The kanji form 悪ぐち is hidden therefore has no associated readings.
+            The kana form あかくち is tagged "nokanji" and therefore has no associated kanji forms.
+            The kana form あかぐち is hidden and therefore has no associated kanji forms.
+        */
         var expectedResults = new List<string>()
         {
+            // あっこう has two kanji forms.
             "悪口|あっこう",
-            "悪口|わるくち",
-            "悪口|わるぐち",
             "惡口|あっこう",
+
+            // わるくち has three kanji forms.
+            "悪口|わるくち",
             "惡口|わるくち",
-            "惡口|わるぐち",
             "わる口|わるくち",
+
+            // わるぐち has three kanji forms.
+            "悪口|わるぐち",
+            "惡口|わるぐち",
             "わる口|わるぐち",
         };
         var dictionaryFilePath = Path.Combine("Resources", "Waruguchi.xml");
